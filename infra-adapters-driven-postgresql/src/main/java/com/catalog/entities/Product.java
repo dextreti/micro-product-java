f94e565f0eeaa16f7b8b9c72ecd78ccdab1c8067
dextreti <dextreti@gmail.com>
2026-01-31 13:45:59 -0800

    relations product categry

--- infra-adapters-driven-postgresql/src/main/java/com/catalog/entities/Product.java
+++ infra-adapters-driven-postgresql/src/main/java/com/catalog/entities/Product.java
@@ -6,6 +6,8 @@ import java.math.BigDecimal;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,4 +27,10 @@ public class Product {
     
     @Column(name = "categoryid", nullable = false)
     public String categoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryid", insertable = false, updatable = false)
+    private Category category; 
+
+
 }
