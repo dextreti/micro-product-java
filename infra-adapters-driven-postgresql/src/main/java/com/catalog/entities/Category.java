9d4976ecd149cb66aeba5e1402f74e9cff254bc6
dextreti <dextreti@gmail.com>
2026-01-31 13:51:02 -0800

    relations product categry

--- infra-adapters-driven-postgresql/src/main/java/com/catalog/entities/Category.java
+++ infra-adapters-driven-postgresql/src/main/java/com/catalog/entities/Category.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 @Entity 
-@Table(name = "Category")
+@Table(name = "category", schema = "product_catalog")
 public class Category {
     @Id
     private String id;
